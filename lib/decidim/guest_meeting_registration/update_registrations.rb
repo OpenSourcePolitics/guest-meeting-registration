de5b25374ac0a58cb42cbff5a4fc8bb8fe4e696c
stephanie rousset <strabon@club-internet.fr>
2025-04-25 09:06:03 +0200

    feat: update lib file

--- lib/decidim/guest_meeting_registration/update_registrations.rb
+++ lib/decidim/guest_meeting_registration/update_registrations.rb
@@ -3,25 +3,24 @@
 module Decidim
   module GuestMeetingRegistration
     module UpdateRegistrations
-      def update_meeting_registrations
-        meeting.registrations_enabled = form.registrations_enabled
-        meeting.registration_form_enabled = form.registration_form_enabled
+      protected
 
+      def attributes
+        extra_params = {}
         if form.registrations_enabled
-          meeting.available_slots = form.available_slots
-          meeting.reserved_slots = form.reserved_slots
-          meeting.registration_terms = form.registration_terms
-          meeting.customize_registration_email = form.customize_registration_email
-
-          meeting.enable_guest_registration = form.enable_guest_registration
-          meeting.enable_registration_confirmation = form.enable_registration_confirmation
-          meeting.enable_cancellation = form.enable_cancellation
-          meeting.disable_account_confirmation = form.disable_account_confirmation
-
-          meeting.registration_email_custom_content = form.registration_email_custom_content if form.customize_registration_email
+          extra_params = {
+            available_slots: form.available_slots,
+            reserved_slots: form.reserved_slots,
+            registration_terms: form.registration_terms,
+            customize_registration_email: form.customize_registration_email,
+            enable_guest_registration: form.enable_guest_registration,
+            enable_registration_confirmation: form.enable_registration_confirmation,
+            enable_cancellation: form.enable_cancellation,
+            disable_account_confirmation: form.disable_account_confirmation
+          }
+          extra_params.merge!(registration_email_custom_content: form.registration_email_custom_content) if form.customize_registration_email
         end
-
-        meeting.save!
+        super.merge(extra_params)
       end
     end
   end
